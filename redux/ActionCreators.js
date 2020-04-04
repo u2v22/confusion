29fdf4bcd9b5b4b708a0223ca96d051556c4284f
Veronica HW <veronica.wajda@gmail.com>
2020-04-04 17:20:37 -0600

    added action creators for comments, dishes, leaders, promotions

---
+++ redux/ActionCreators.js
@@ -0,0 +1,158 @@
+import * as ActionTypes from './ActionTypes';
+import { baseUrl } from '../shared/baseUrl';
+
+export const fetchComments = () => (dispatch) => {
+  return fetch(`baseUrl${comments}`)
+    .then(response => {
+      if(response.ok){
+        return response;
+      }
+      else {
+        let error = new Error(`Error ${response.status}: ${response.statusText}`);
+        error.response = response;
+        throw error;
+      }
+    },
+    error => {
+      let errMsg = new Error(error.message)
+      throw errMsg;
+    })
+    .then(response => response.json())
+    .then(comments => dispatch(addComments(comments)))
+    .catch(error => dispatch(commentsFailed(error.message)))
+}
+
+
+export const commentsFailed = (errMsg) => ({
+  type: ActionTypes.COMMENTS_FAILED,
+  payload: errMsg
+});
+
+export const addComments = (comments) => ({
+  type: ActionTypes.ADD_COMMENTS,
+  payload: comments
+});
+
+
+
+
+export const fetchDishes = () => (dispatch) => {
+  dispatch(dishesLoading());
+
+  return fetch(`baseUrl${comments}`)
+    .then(response => {
+      if(response.ok){
+        return response;
+      }
+      else {
+        let error = new Error(`Error ${response.status}: ${response.statusText}`);
+        error.response = response;
+        throw error;
+      }
+    },
+    error => {
+      let errMsg = new Error(error.message)
+      throw errMsg;
+    })
+    .then(response => response.json())
+    .then(dishes => dispatch(addDishes(dishes)))
+    .catch(error => dispatch(dishesFailed(error.message)))
+}
+
+export const dishesLoading = () => ({
+  type: ActionTypes.DISHES_LOADING,
+});
+
+export const dishesFailed = (errMsg) => ({
+  type: ActionTypes.DISHES_FAILED,
+  payload: errMsg
+});
+
+export const addComments = (dishes) => ({
+  type: ActionTypes.ADD_DISHES,
+  payload: dishes
+});
+
+
+
+
+export const fetchPromotions = () => (dispatch) => {
+  dispatch(promotionsLoading());
+
+  return fetch(`baseUrl${promotions}`)
+    .then(response => {
+      if(response.ok){
+        return response;
+      }
+      else {
+        let error = new Error(`Error ${response.status}: ${response.statusText}`);
+        error.response = response;
+        throw error;
+      }
+    },
+    error => {
+      let errMsg = new Error(error.message)
+      throw errMsg;
+    })
+    .then(response => response.json())
+    .then(promotions => dispatch(addPromotions(promotions)))
+    .catch(error => dispatch(promotionsFailed(error.message)))
+}
+
+export const promotionsLoading = () => ({
+  type: ActionTypes.PROMOTIONS_LOADING,
+});
+
+export const promotionsFailed = (errMsg) => ({
+  type: ActionTypes.PROMOTIONS_FAILED,
+  payload: errMsg
+});
+
+export const addPromotions = (promotions) => ({
+  type: ActionTypes.ADD_PROMOTIONS,
+  payload: promotions
+});
+
+
+
+
+export const fetchLeaders = () => (dispatch) => {
+  dispatch(leadersLoading());
+
+  return fetch(`baseUrl${leaders}`)
+    .then(response => {
+      if(response.ok){
+        return response;
+      }
+      else {
+        let error = new Error(`Error ${response.status}: ${response.statusText}`);
+        error.response = response;
+        throw error;
+      }
+    },
+    error => {
+      let errMsg = new Error(error.message)
+      throw errMsg;
+    })
+    .then(response => response.json())
+    .then(leaders => dispatch(addLeaders(leaders)))
+    .catch(error => dispatch(leadersFailed(error.message)))
+}
+
+export const leadersLoading = () => ({
+  type: ActionTypes.LEADERS_LOADING,
+});
+
+export const leadersFailed = (errMsg) => ({
+  type: ActionTypes.LEADERS_FAILED,
+  payload: errMsg
+});
+
+export const addLeaderss = (leaders) => ({
+  type: ActionTypes.ADD_LEADERS,
+  payload: leaders
+});
+
+
+
+
