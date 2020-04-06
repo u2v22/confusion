25d94e6b1636002b767820fc2d9d35370f249db3
Veronica HW <veronica.wajda@gmail.com>
2020-04-06 10:09:07 -0600

    added action creators for favorites

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -6,6 +6,7 @@ import { dishes } from './dishes';
 import { comments } from './comments';
 import { promotions } from './promotions';
 import { leaders } from './leaders';
+import { favorites } from './favorites';
 
 export const ConfigureStore = () => {
   const store = createStore(
@@ -13,7 +14,8 @@ export const ConfigureStore = () => {
         dishes,
         comments,
         promotions,
-        leaders
+        leaders,
+        favorites
     }),
     applyMiddleware(thunk, logger)
   );
