70e4261d46145f29b787fff55a68118c108c5679
Veronica HW <veronica.wajda@gmail.com>
2020-04-07 15:58:26 -0600

    added Animate view - horizontal translation

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, ScrollView, View } from 'react-native';
+import { Text, ScrollView, View, Animated, Easing } from 'react-native';
 import { Card } from 'react-native-elements';
 
 import AboutUs from './AboutUs';
@@ -53,23 +53,72 @@ function RenderItem(props){
 }
 
 class Home extends Component {
+  constructor(props){
+    super(props);
+
+    this.animatedValue = new Animated.Value(0);
+  }
+
+  componentDidMount(){
+    this.animate();
+  }
+
+  animate(){
+    this.animatedValue.setValue(0);
+    Animated.timing(
+      this.animatedValue,
+      {
+        toValue: 8,
+        duration: 8000,
+        easing: Easing.linear
+      }
+    ).start(() => this.animate())
+  }
+
+
   render(){
+    const xPos1 = this.animatedValue.interpolate({
+      inputRange: [0, 1, 3, 5, 8],
+      outputRange: [1200, 600, 0, -600, -1200],
+    })
+
+    const xPos2 = this.animatedValue.interpolate({
+      inputRange: [0, 2, 4, 6, 8],
+      outputRange: [1200, 600, 0, -600, -1200],
+    })
+
+    const xPos3 = this.animatedValue.interpolate({
+      inputRange: [0, 3, 5, 7, 8],
+      outputRange: [1200, 600, 0, -600, -1200],
+    })
+
+    const style = {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'center'
+    }
+
     return(
       <ScrollView>
-        <RenderItem item={this.props.dishes.dishes.filter((dish) => dish.featured )[0]}
-                    isLoading={this.props.dishes.isLoading}
-                    errMsg={this.props.dishes.errMsg}
-                    onPress={() => alert('todo')}/>
-        <RenderItem item={this.props.promotions.promotions.filter((promo) => promo.featured )[0]}
-                    isLoading={this.props.promotions.isLoading}
-                    errMsg={this.props.promotions.errMsg}
-                    onPress={() => alert('todo')}/>
-        <RenderItem item={this.props.leaders.leaders.filter((lead) => lead.featured )[0]}
-                    isLoading={this.props.leaders.isLoading}
-                    errMsg={this.props.leaders.errMsg}
-                    onPress={() => alert('todo')}/>
+        <View style={style}>
+          <Animated.View style={{ width: '100%', transform: [{ translateX: xPos1 }] }}>
+            <RenderItem item={this.props.dishes.dishes.filter((dish) => dish.featured )[0]}
+                        isLoading={this.props.dishes.isLoading}
+                        errMsg={this.props.dishes.errMsg} />
+          </Animated.View>
+          <Animated.View style={{ width: '100%', transform: [{ translateX: xPos2 }] }}>
+            <RenderItem item={this.props.promotions.promotions.filter((promo) => promo.featured )[0]}
+                        isLoading={this.props.promotions.isLoading}
+                        errMsg={this.props.promotions.errMsg} />
+          </Animated.View>
+          <Animated.View style={{ width: '100%', transform: [{ translateX: xPos3 }] }}>
+            <RenderItem item={this.props.leaders.leaders.filter((lead) => lead.featured )[0]}
+                        isLoading={this.props.leaders.isLoading}
+                        errMsg={this.props.leaders.errMsg} />
+          </Animated.View>
+        </View>
       </ScrollView>
-    )
+    );
   }
 }
 
